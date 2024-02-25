586ca92c19da2b25ea67dd4ef6a2823af5a29e0a
sarahschoonmaker <srschoonmaker@gmail.com>
2024-02-25 09:51:48 -0500

    sprint2

--- src/main/com/adventure/world/objects/Hole.java
+++ src/main/com/adventure/world/objects/Hole.java
@@ -3,18 +3,17 @@ package main.com.adventure.world.objects;
 import main.com.adventure.world.objects.keys.Key;
 
 public class Hole {
-
+    private HoleContent holeContent;
     /**
      * Creates a hole with the given content.
      * @param content - the item that is covered by the hole.
      */
     public Hole(Key content) {
-
+        holeContent = new HoleContent(content);
     }
 
-    public boolean isCovered() {
-        //TODO This value should come from HoleContent
-        return false;
+    public boolean isCovered () {
+        return holeContent.isCovered();
     }
 
     /**
@@ -22,14 +21,18 @@ public class Hole {
      */
     public void dig() {
         //TODO this function should update HoleContent's isCovered property.
+        holeContent.setIsCovered(false);
     }
 
     /**
      * If the HoleContent is uncovered, return the key. Otherwise, return null.
      * @return the key if the hold is uncovered.
      */
-    private Key getKeyIfPossible() {
+    private Key getKeyIfPossible () {
         //TODO Get the HoleContent's content only if the hole is uncovered.
+        if (holeContent.isCovered()) {
+            return holeContent.getKey();
+        }
         return null;
     }
 
---
+++ src/main/com/adventure/world/objects/HoleContent.java
@@ -0,0 +1,26 @@
+package main.com.adventure.world.objects;
+
+import main.com.adventure.world.objects.keys.Key;
+
+public class HoleContent {
+    private Key content = new Key();
+    private boolean isCovered = true;
+
+    /**
+     * Create what goes in a hole.
+     * (hint: It will always be a key)
+     * @param key - of type Key.
+     */
+    public HoleContent(Key key) {
+        content = key;
+    }
+    public void setIsCovered(boolean isCovered) {
+        this.isCovered = isCovered;
+    }
+    public boolean isCovered() {
+        return isCovered;
+    }
+    public Key getKey() {
+        return content;
+    }
+}
